61fbba7ebcb8b3ee63bb000cb9c6247795eb0d36
Pablo Solano <pasolano@email.wm.edu>
2020-10-29 15:33:30 -0400

    fixed ampersand?

--- static/js/home.js
+++ static/js/home.js
@@ -3,8 +3,8 @@ $(function() {
   $(".typed").typed({
     strings: [
       "stat pablo.human<br/>" + 
-      "><span class='caret'>$</span> school: computer science major at william & mary<br/> ^100" +
-      "><span class='caret'>$</span> honors: <a href='https://www.wm.edu/as/1693scholars/current/solano_p/index.php'>stamps 1693 scholar</a>, william & mary scholar, james monroe scholar<br/>" +
+      "><span class='caret'>$</span> school: computer science major at william \& mary<br/> ^100" +
+      "><span class='caret'>$</span> honors: <a href='https://www.wm.edu/as/1693scholars/current/solano_p/index.php'>stamps 1693 scholar</a>, william \& mary scholar, james monroe scholar<br/>" +
       "><span class='caret'>$</span> languages: python, javascript, c++, java<br/> ^100" +
       "><span class='caret'>$</span> bilingual/biliterate: english/spanish<br/> ^300"/*
       "><span class='caret'>$</span> <a href='/timeline'>timeline</a> <a href='http://www.github.com/crearo/'>github</a> <a href='http://in.linkedin.com/in/bhardwajrish/'>linkedin</a> <a href='http://bhardwajrish.blogspot.com/'>blog</a><br/>"*/
